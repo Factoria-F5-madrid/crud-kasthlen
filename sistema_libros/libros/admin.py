18fd9ca3ac077e4aeef242f979d97368dcafaf03
kasthlen <kasthlen97@gmail.com>
2025-07-14 10:29:18 +0200

    update info

---
+++ sistema_libros/libros/admin.py
@@ -0,0 +1,10 @@
+# sistema_libros/libros/admin.py
+# Register your models here.
+from django.contrib import admin
+from .models import Libro
+
+@admin.register(Libro)
+class LibroAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'autor', 'fecha_publicacion', 'isbn')
+    search_fields = ('titulo', 'autor', 'isbn')
+    list_filter = ('fecha_publicacion',)
